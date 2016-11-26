94cf16210f241bede3f80a98f74a84f63d894c13
Bill Brubaker <brubaker.bill@gmail.com>
2016-11-26 16:01:02 -0600

    Added comments

--- StrangerLightsHash.ino
+++ StrangerLightsHash.ino
@@ -1,6 +1,10 @@
 #define HASH 63
 
-
+/*
+ * NOTE: Re-order numbers 0-25 according the 
+ * arrangement of the physical lights, but 
+ * DO NOT change their position in the array.
+ */
 uint8_t lights[64] = {
     0,
     0,
@@ -9,7 +13,8 @@ uint8_t lights[64] = {
     0,  1,  2,  3,  4,  5, 6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
     0, 0, 0, 0
 };
-    
+
+//This array for debug only, to simulate char for actual lights.
 char alphabet[64] = {
     ' ',
     ' ',
@@ -19,7 +24,7 @@ char alphabet[64] = {
     '_', '_', '_', '_'
 };
 
-String sentence = "suck it trebeck";
+String sentence = "suck it trebeck"; //Debug/test only.
     
 void setup() {
   // put your setup code here, to run once:
